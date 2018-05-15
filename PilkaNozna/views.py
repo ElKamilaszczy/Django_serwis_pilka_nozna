6ddd0468619d2be421dcb8141f2c05fd3bd98aa1
kucharz96 <kucharz96@wp.pl>
2018-05-15 20:06:49 +0200

    final

--- PilkaNozna/views.py
+++ PilkaNozna/views.py
@@ -4,14 +4,38 @@ from .models import Liga,Klub,Mecz,Pilkarz,Statystyki_gracza
 from .forms import PostForm
 from django.shortcuts import redirect
 
-
-
 def ligi(request):
     latest_question = Liga.objects.order_by('nazwa_ligi')
 
     context = {'latest_question': latest_question}
     return render(request, 'PilkaNozna/index.html', context)
 
+def gole(id_meczu,id_klubu):
+    pilkarz = Pilkarz.objects.filter(id_klubu=id_klubu)
+    mecz = Mecz.objects.get(id_meczu=id_meczu)
+    gole = 0
+    gole_p = 0
+    staty = Statystyki_gracza.objects.filter(id_meczu=id_meczu)
+    if id_klubu == mecz.id_klubu1.id_klubu:
+        przeciwnik = mecz.id_klubu2.id_klubu
+    else:
+        przeciwnik = mecz.id_klubu1.id_klubu
+    pilkarz_p = Pilkarz.objects.filter(id_klubu=przeciwnik)
+
+    for d in pilkarz:
+        for e in staty:
+            if e.id_pilkarza.id_pilkarza == d.id_pilkarza:
+                gole += e.gole
+
+    for d in pilkarz_p:
+        for e in staty:
+            if d.id_pilkarza == e.id_pilkarza.id_pilkarza:
+                gole_p += e.gole
+
+    tab = []
+    tab.insert(0,gole)
+    tab.insert(1,gole_p)
+    return tab
 
 
 def detail(request, id_ligi):
@@ -22,8 +46,7 @@ def detail(request, id_ligi):
     for a in kl:
 
         mecz = Mecz.objects.filter(id_klubu1=a.id_klubu) | Mecz.objects.filter(id_klubu2=a.id_klubu)
-        pilkarz = Pilkarz.objects.filter(id_klubu=a.id_klubu)
-        for b in range (8):
+        for b in range (9):
             if b==0:
                 abc[var][b]=var
 
@@ -32,36 +55,43 @@ def detail(request, id_ligi):
             if b==2:
                 abc[var][b]=mecz.count()
             if b==3:
-
                 for c in mecz:
-                    gole = 0
-                    gole_p = 0
-                    staty = Statystyki_gracza.objects.filter(id_meczu=c.id_meczu)
-                    if a.id_klubu == c.id_klubu1.id_klubu:
-                        przeciwnik = c.id_klubu2.id_klubu
-                    else:
-                        przeciwnik = c.id_klubu1.id_klubu
-                    pilkarz_p =  Pilkarz.objects.filter(id_klubu=przeciwnik)
+                    temp = gole(c.id_meczu,a.id_klubu)
 
+                    if temp[0] > temp[1]:
 
-                    for d in pilkarz:
-                        for e in staty:
-                            if e.id_pilkarza.id_pilkarza == d.id_pilkarza:
-                                gole += e.gole
+                        abc[var][b] += 3
+                    if temp[0] == temp[1]:
+                        abc[var][b] += 1
 
-                    for d in pilkarz_p:
-                        for e in staty:
-                            if d.id_pilkarza == e.id_pilkarza.id_pilkarza:
-                                gole_p +=e.gole
+            if b==4:
+                for c in mecz:
+                    temp = gole(c.id_meczu,a.id_klubu)
+                    if temp[0] > temp[1]:
+                        abc[var][b] += 1
+            if b==5:
+                for c in mecz:
+                    temp = gole(c.id_meczu,a.id_klubu)
+                    if temp[0] < temp[1]:
+                        abc[var][b] += 1
+            if b==6:
+                for c in mecz:
+                    temp = gole(c.id_meczu,a.id_klubu)
+                    if temp[0] == temp[1]:
+                        abc[var][b] += 1
+            if b==7:
+                for c in mecz:
+                    temp = gole(c.id_meczu,a.id_klubu)
+                    abc[var][b] += temp[0]
+            if b==8:
+                for c in mecz:
+                    temp = gole(c.id_meczu,a.id_klubu)
+                    abc[var][b] += temp[1]
 
-                    if gole > gole_p:
-                        abc[var][b]+=3
-                    if gole == gole_p:
-                        abc[var][b]+=1
 
         var += 1
 
-
+    np.sort(abc, axis=0)
 
 
     context = {'lg': lg,'kl':kl,'abc':abc,'mecz':mecz}
